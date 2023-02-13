70fba1064ab39970b32d7c13573a8375e28fa725
Santhosh <santhoshhariharan1393@gmail.com>
2023-02-13 10:19:54 +0530

    Final commit

---
+++ check.py
@@ -0,0 +1,28 @@
+from io import StringIO
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+def is_pdf_full_blank(file_path):
+    manager = PDFResourceManager()
+    output = StringIO()
+    converter = TextConverter(manager, output, laparams=LAParams())
+    interpreter = PDFPageInterpreter(manager, converter)
+
+    with open(file_path, "rb") as f:
+        for page in PDFPage.get_pages(f):
+            interpreter.process_page(page)
+
+    text = output.getvalue().strip()
+    converter.close()
+    output.close()
+
+    return text == ""
+
+if __name__ == "__main__":
+    file_path = "AIML.pdf"
+    if is_pdf_full_blank(file_path):
+        print("The PDF file is full blank.")
+    else:
+        print("The PDF file is not full blank.")
